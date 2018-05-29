3fdd7eacb265f2030fcd916f39ba66bcc014b3ff
millionairetai <millionairetai@gmail.com>
2018-05-29 16:41:49 +0700

    commit storage

---
+++ tests/Unit/DeployTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class DeployTest extends TestCase
+{
+    /**
+     * A check test example.
+     *
+     * @return void
+     */
+    public function testcheckDeploy()
+    {
+        $flag = true;
+        $this->assertTrue($flag);
+    }
+}
