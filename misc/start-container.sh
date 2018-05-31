167e8293b82a1626519efb4baff8a4b91f7ae6fd
Tai test <millionairetai@gmail.com>
2018-05-31 10:57:59 +0700

    add docker file and shell script

---
+++ misc/start-container.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copyright (c) 2018 Project. All rights reserved.
+# ---------------------------------------------------------------------------------------
+# NOTICE:  All information contained herein is, and remains
+# the property of Vietnam and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Vietnam
+# and its suppliers and may be covered by Vietnamese Law,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Vietnam.
+# ---------------------------------------------------------------------------------------
+# Author:
+#       Tai Tran <tai.tran@gmail.com>
+
+
+#start apache2
+service apache2 start 
+
+#start mysql
+service mysql start 
+
+/bin/bash
