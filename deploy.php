868b400dd177a25ec7b494cd6ba551c087ca1891
Tai test <millionairetai@gmail.com>
2018-05-31 14:45:29 +0700

    test protect branch

--- deploy.php
+++ deploy.php
@@ -25,34 +25,36 @@ EOT;
 
 // Check whether client is allowed to trigger an update
 
-//$allowed_ips = array(
-//    '207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
-//    '195.37.139.','193.174.' // VZG
-//);
-//$allowed = false;
-//
-//$headers = apache_request_headers();
-//
-//if (@$headers["X-Forwarded-For"]) {
-//    $ips = explode(",",$headers["X-Forwarded-For"]);
-//    $ip  = $ips[0];
-//} else {
-//    $ip = $_SERVER['REMOTE_ADDR'];
-//}
-//
-//foreach ($allowed_ips as $allow) {
-//    if (stripos($ip, $allow) !== false) {
-//        $allowed = true;
-//        break;
-//    }
-//}
-//
-//if (!$allowed) {
-//    header('HTTP/1.1 403 Forbidden');
-//    echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-//    echo "</pre>\n</body>\n</html>";
-//    exit;
-//}
+$allowed_ips = array(
+    '207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
+    '195.37.139.','193.174.' // VZG
+);
+$allowed = false;
+
+$headers = apache_request_headers();
+
+if (@$headers["X-Forwarded-For"]) {
+    $ips = explode(",",$headers["X-Forwarded-For"]);
+    $ip  = $ips[0];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+echo '############' . $ip . '##########';
+
+foreach ($allowed_ips as $allow) {
+    if (stripos($ip, $allow) !== false) {
+        $allowed = true;
+        break;
+    }
+}
+
+if (!$allowed) {
+    header('HTTP/1.1 403 Forbidden');
+    echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
+    echo "</pre>\n</body>\n</html>";
+    exit;
+}
 
 flush();
 
